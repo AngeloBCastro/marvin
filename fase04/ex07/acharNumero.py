b59e71800cb043208217f2881f6e984d5bb7b5bf
AngeloBCastro <52722939+AngeloBCastro@users.noreply.github.com>
2019-07-30 20:54:38 -0300

    achar numero

---
+++ fase04/ex07/acharNumero.py
@@ -0,0 +1,9 @@
+def acharNumero(num):
+    if( num < 5 ):
+        return "a"
+    if( num >= 5 and num <= 10 ):
+        return 'b'
+    if( num >= 10 and num <= 100 ):
+        return "c"
+    if( num > 100):
+        return "d"
