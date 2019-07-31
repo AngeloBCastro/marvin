0bc3216e69d03c971928204e33d809208ce34a58
AngeloBCastro <52722939+AngeloBCastro@users.noreply.github.com>
2019-07-31 13:25:55 -0300

    Alfabeto militar

---
+++ fase04/ex08/alfabetoMilitar.py
@@ -0,0 +1,13 @@
+def alfabetoMilitar(vogal):
+    if vogal in ["a", "A"]:
+        return "alpha"
+    if vogal in ["e", "E"]:
+        return "echo"
+    if vogal in ["i", "I"]:
+        return "india"
+    if vogal in ["o", "O"]:
+        return "oscar"
+    if vogal in ["u", "U"]:
+        return "uniform"
+    else:
+        return "false"
